7226341a50f7934165eb487e59f0efe0d651785f
陈洋 <xiazichenxi@126.com>
2020-06-24 16:19:42 +0800

    Update pull-image.sh

--- pull-image.sh
+++ pull-image.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# leo 20190923
+# leo 20200624
 # 镜像一键下载
 
 images=(
@@ -9,20 +9,21 @@ images=(
   #calico/kube-controllers:v3.10.0
   #calico/pod2daemon-flexvol:v3.10.0  
   
-  registry.cn-shenzhen.aliyuncs.com/k8s-install-kubeadm/calico_node:v3.10.0
-  registry.cn-shenzhen.aliyuncs.com/k8s-install-kubeadm/calico_cni:v3.10.0
-  registry.cn-shenzhen.aliyuncs.com/k8s-install-kubeadm/calico_kube-controllers:v3.10.0
-  registry.cn-shenzhen.aliyuncs.com/k8s-install-kubeadm/calico_pod2daemon-flexvol:v3.10.0
+  #registry.cn-shenzhen.aliyuncs.com/k8s-install-kubeadm/calico_node:v3.10.0
+  #registry.cn-shenzhen.aliyuncs.com/k8s-install-kubeadm/calico_cni:v3.10.0
+  #registry.cn-shenzhen.aliyuncs.com/k8s-install-kubeadm/calico_kube-controllers:v3.10.0
+  #registry.cn-shenzhen.aliyuncs.com/k8s-install-kubeadm/calico_pod2daemon-flexvol:v3.10.0
   
-  registry.cn-hangzhou.aliyuncs.com/google_containers/kube-apiserver:v1.16.0
-  registry.cn-hangzhou.aliyuncs.com/google_containers/kube-controller-manager:v1.16.0
-  registry.cn-hangzhou.aliyuncs.com/google_containers/kube-proxy:v1.16.0
-  registry.cn-hangzhou.aliyuncs.com/google_containers/kube-scheduler:v1.16.0
-  registry.cn-hangzhou.aliyuncs.com/google_containers/etcd:3.3.15-0
-  registry.cn-hangzhou.aliyuncs.com/google_containers/coredns:1.6.2
-  registry.cn-hangzhou.aliyuncs.com/google_containers/pause:3.1
+  # 使用kubeadm config images list --kubernetes-version=v1.18.2 查看各个组件的版本，然后更新到下面
+  registry.cn-hangzhou.aliyuncs.com/google_containers/kube-apiserver-amd64:v1.18.2
+  registry.cn-hangzhou.aliyuncs.com/google_containers/kube-controller-manager-amd64:v1.18.2
+  registry.cn-hangzhou.aliyuncs.com/google_containers/kube-proxy-amd64:v1.18.2
+  registry.cn-hangzhou.aliyuncs.com/google_containers/kube-scheduler-amd64:v1.18.2
+  registry.cn-hangzhou.aliyuncs.com/google_containers/etcd-amd64:3.4.3-0
+  registry.cn-hangzhou.aliyuncs.com/google_containers/coredns:1.6.7
+  registry.cn-hangzhou.aliyuncs.com/google_containers/pause:3.2
 )
 
 for imageName in ${images[@]} ; do
-    docker pull $imageName
+    docker image pull $imageName
 done
